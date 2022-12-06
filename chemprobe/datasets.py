90d351b124336b1c1c60aa97c9ba79c78b32cd3f
Will Connell <wconnell93@gmail.com>
2022-12-06 15:22:31 -0800

    split datamodule into train/predict

--- chemprobe/datasets.py
+++ chemprobe/datasets.py
@@ -30,6 +30,7 @@ import pytorch_lightning as pl
 
 # Custom
 from chemprobe.chem import generate_doses
+from chemprobe.bio import PROTCODE_GENES
 
 
 ###########################################################################################################################################
@@ -79,8 +80,6 @@ class ChemProbeDataModule(pl.LightningDataModule):
         batch_size=1024,
         onehot_cpds=False,
         permute_labels=False,
-        pred_cells=None, 
-        pred_cpds=None,
         num_workers=0,
     ):
         super().__init__()
@@ -89,8 +88,6 @@ class ChemProbeDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.onehot_cpds = onehot_cpds
         self.permute_labels = permute_labels
-        self.pred_cells = pred_cells
-        self.pred_cpds = pred_cpds
         self.num_workers = num_workers
         
     @staticmethod
@@ -102,15 +99,6 @@ class ChemProbeDataModule(pl.LightningDataModule):
         parser.add_argument("--num_workers", type=int, default=0)
         parser.add_argument("--permute_labels", action="store_true")
         parser.add_argument("--onehot_cpds", action="store_true", help="One-hot encode compounds")
-        parser.add_argument(
-            "--pred_cells",
-            type=Path,
-            default=None,
-            help="Path to csv of samples for predictions.",
-        )
-        parser.add_argument(
-            "--pred_cpds", nargs="+", default=None, help="Compounds to make predictions for."
-        )
         return parent_parser
 
     # When doing distributed training, Datamodules have two optional arguments for
@@ -173,27 +161,7 @@ class ChemProbeDataModule(pl.LightningDataModule):
             raise NotImplementedError
 
         if stage == "predict":
-            # read in custom supplied file
-            self.pred_cells = pd.read_csv(self.pred_cells, index_col=0)
-
-            if self.pred_cpds is not None:
-                self.cpds = self.cpds.loc[self.pred_cpds]
-                print("Predicting on supplied CTRP compounds...")
-            else:
-                print("Predicting on all CTRP compounds...")
-            
-            # create samples across different concentrations
-            self.pred_metadata = generate_doses(
-                self.cpds.index.unique(), self.pred_cells.index.unique()
-            )
-            self.pred_metadata["viability"] = np.nan  # placeholder
-
-            self.pred_dataset = ChemProbeDataset(
-                self.pred_metadata,
-                self.pred_cells,
-                self.cpds,
-            )
-            return self.pred_dataset
+            raise NotImplementedError
 
     def train_dataloader(self):
         sampler = BatchSampler(RandomSampler(self.train_dataset), self.batch_size, drop_last=False)
@@ -221,6 +189,130 @@ class ChemProbeDataModule(pl.LightningDataModule):
         raise NotImplementedError
 
     def predict_dataloader(self):
+        raise NotImplementedError
+
+
+class ChemProbePredictDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        data_path, 
+        cpds=None,
+        batch_size=1024,
+        num_workers=0,
+        pin_memory=True,
+    ):
+        super().__init__()
+        self.data_path = Path(data_path)
+        self.cpds = cpds
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.pin_memory = pin_memory
+        self.PROTCODE_GENES = PROTCODE_GENES
+        
+    @staticmethod
+    def add_argparse_args(parent_parser, **kwargs):
+        parser = parent_parser.add_argument_group("CTRPDataModule")
+        parser.add_argument("--data_path", type=Path, required=True)
+        parser.add_argument(
+            "--cpds", nargs="+", default=None, help="Compounds to make predictions for."
+        )
+        parser.add_argument("--batch_size", type=int, default=1024)
+        parser.add_argument("--num_workers", type=int, default=0)
+        return parent_parser
+
+    # When doing distributed training, Datamodules have two optional arguments for
+    # granular control over download/prepare/splitting data:
+    def prepare_data(self):
+        pass
+
+    # OPTIONAL, called for every GPU/machine (assigning state is OK)
+    def setup(self, stage):
+
+        if stage == "fit":
+            raise NotImplementedError
+
+        if stage == "test":
+            raise NotImplementedError
+
+        if stage == "predict":
+            # read in custom supplied file
+            self.pred_cells = pd.read_csv(self.data_path.joinpath("cells.csv.gz"), index_col=0)
+            self.pred_cells = self.impute_genes(self.pred_cells)
+            self.pred_cells = self.filter_genes(self.pred_cells)
+            self.pred_cells = pd.DataFrame(
+                StandardScaler().fit_transform(self.pred_cells),
+                index=self.pred_cells.index,
+                columns=self.pred_cells.columns
+            )
+
+            # read in CTRP data
+            cpds = pd.read_csv(self.data_path.joinpath("cpds.csv.gz"), index_col=0)
+
+            if self.cpds is not None:
+                print(f"Predicting on supplied CTRP compounds: {self.cpds}")
+                self.cpds = cpds.loc[self.cpds]
+            else:
+                print("Predicting on all CTRP compounds")
+                self.cpds = cpds
+            
+            # create samples across different concentrations
+            self.pred_metadata = generate_doses(
+                self.cpds.index.unique(), self.pred_cells.index.unique()
+            )
+            self.pred_metadata["viability"] = np.nan  # placeholder
+
+            self.pred_dataset = ChemProbeDataset(
+                self.pred_metadata,
+                self.pred_cells,
+                self.cpds,
+            )
+            return self.pred_dataset
+
+    def impute_genes(self, df, missing_tolerance=0.15):
+        missing = set(self.PROTCODE_GENES) - set(df.columns)
+        if missing:
+            frac = len(missing) / len(df.columns)
+            if frac > missing_tolerance:
+                raise ValueError(f"Missing {frac*100:.2f}% of genes, missing tolerance is {missing_tolerance*100:.2f}%")
+            else:
+                print(f"Missing {frac*100:.2f}% of genes, zero imputing")
+                impute_df = pd.DataFrame(
+                    np.zeros((len(df), len(missing))), index=df.index, columns=list(missing)
+                )
+                df = pd.concat((df, impute_df), axis=1)
+        return df
+
+    def filter_genes(self, df, filter_by="var"):
+        # subset to protcode genes
+        df = df[self.PROTCODE_GENES]
+        # filter out duplicate genes
+        if df.columns.has_duplicates:
+            dup = df.columns[df.columns.duplicated()]
+            print(f"Duplicate genes: {dup}\n")
+            if len(dup) > 100:
+                Warning("More than 100 duplicate genes, this may be a problem")
+            df = df.T
+            if filter_by == "mean":
+                print(f"Keeping gene duplicates with highest average expression")
+                df[filter_by] = df.mean(axis=1)
+            if filter_by == "var":
+                print(f"Keeping gene duplicates with highest expression variance")
+                df[filter_by] = df.var(axis=1)
+            df = df.sort_values(by=filter_by, ascending=False)
+            df = df[~df.index.duplicated(keep="first")]
+            df = df.drop(columns=[filter_by]).T
+        return df
+
+    def train_dataloader(self):
+        raise NotImplementedError
+
+    def val_dataloader(self):
+        raise NotImplementedError
+
+    def test_dataloader(self):
+        raise NotImplementedError
+
+    def predict_dataloader(self):
         sampler = BatchSampler(SequentialSampler(self.pred_dataset), self.batch_size, drop_last=False)
         return DataLoader(
             self.pred_dataset,
@@ -228,5 +320,5 @@ class ChemProbeDataModule(pl.LightningDataModule):
             batch_sampler=None,
             batch_size=None,
             num_workers=self.num_workers,
-            pin_memory=True,
+            pin_memory=self.pin_memory,
         )
