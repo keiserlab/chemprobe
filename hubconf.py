39c49d04e1882009b72d3409fd824300dc8c8982
Will Connell <wconnell93@gmail.com>
2022-12-08 14:23:10 -0800

    fix paths

--- hubconf.py
+++ hubconf.py
@@ -7,13 +7,14 @@ def ChemProbeEnsemble():
     """"
     ChemProbeEnsemble model, 5 models trained on separate data folds.
     """
+    model_dir = Path(__file__).resolve().parent.joinpath("data/weights")
     try:
-        model = torch.load("data/weights/chemprobe-ensemble.pt")
+        model = torch.load(model_dir.joinpath("chemprobe-ensemble.pt"))
     except:
         print('Downloading model weights...')
-        model_dir = Path('data/weights/')
         model_dir.mkdir(parents=True, exist_ok=False)
-        torch.hub.download_url_to_file("https://drive.google.com/drive/folders/1b6qaDldujJFrW_jkgs-IRsNKyvkBVzLa?usp=sharing", "data/weights/chemprob-ensemble.pt")
+        # TODO store model somewhere else, not google drive
+        torch.hub.download_url_to_file("https://drive.google.com/file/d/1DYcE46rvbcLgIUyLOI1yD8vbxTk827_8", model_dir.joinpath("chemprobe-ensemble.pt"))
         model = torch.load("data/weights/chemprobe-ensemble.pt")
     
     return model
