82c06f301b5c7d5fe849748b6a181fac76906206
Will Connell <wconnell93@gmail.com>
2023-01-09 16:03:55 -0800

    hosted model weights

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,8 @@
-dependencies = ['pytorch_lightning', 'captum']
+dependencies = ['pytorch_lightning', 'captum', 'gdown']
 
 from pathlib import Path
 import torch
+import gdown
 from chemprobe.models import ChemProbeEnsemble as _ChemProbeEnsemble
 
 def ChemProbeEnsemble(**kwargs):
@@ -9,17 +10,17 @@ def ChemProbeEnsemble(**kwargs):
     ChemProbeEnsemble model, 5 models trained on separate data folds.
     attribute: (bool) if True returns attributions for each prediction (default: False)
     """
-    model_dir = Path(torch.hub.get_dir())
-    models = [model_dir.joinpath(f"chemprobe-ensemble/fold={i}.pt") for i in range(5)]
+    model_dir = Path(torch.hub.get_dir()).joinpath("data/weights")
+    models = [model_dir.joinpath(f"fold={i}.pt") for i in range(5)]
     try:
         model = _ChemProbeEnsemble(models, **kwargs)
     except:
         # raise ValueError
         print('Downloading model weights...')
-        model_dir.joinpath("chemprobe-ensemble").mkdir(parents=True, exist_ok=False)
+        model_dir.mkdir(parents=True, exist_ok=False)
         # TODO TEST and store model online
-        for i in range(5):
-            torch.hub.download_url_to_file(f"url/fold={i}.pt", model_dir.joinpath(f"chemprobe-ensemble/fold={i}.pt"))
+        url = "https://drive.google.com/drive/folders/1bSVeMoFu-9h7gj8ISD9yt8kC_ks9_-Y-?usp=share_link"
+        gdown.download_folder(url, output=model_dir, quiet=True, use_cookies=False)
         model = _ChemProbeEnsemble(models, **kwargs)
     
     return model
